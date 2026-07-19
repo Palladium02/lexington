5efc0a55017df3f93c561b93a605ff710d788957
Palladium02 <seanmeyer544@gmail.com>
2026-07-19 14:12:29 +0200

    added ascii_alpha matcher

--- src/combinators/ascii.rs
+++ src/combinators/ascii.rs
@@ -1,7 +1,7 @@
 use crate::{
     combinators::predicate::predicate,
     input::Input,
-    matcher::{Matcher, MatcherBase},
+    matcher::{Matcher, MatcherBase, MatcherExt},
 };
 
 /// Returns a matcher compatible with inputs that use `char` as symbol. The matcher tests if a given character is ascii lowercase.
@@ -20,4 +20,13 @@ where
     I: Input<Symbol = char> + Sized + 'static,
 {
     predicate(|symbol: char| symbol.is_ascii_uppercase())
+}
+
+/// Returns a matcher compatible with inputs that use `char` as symbol. The matcher succeeds on ascii upper- and lowercase characters.
+#[must_use]
+pub fn ascii_alpha<I>() -> impl Matcher<I> + MatcherBase + Sized + 'static
+where
+    I: Input<Symbol = char> + Sized + 'static,
+{
+    ascii_lower().or(ascii_upper())
 }
