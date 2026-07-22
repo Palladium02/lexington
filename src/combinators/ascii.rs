982e05fc37e011eadb341a9663c96264c35ae7e2
Sean Meyer <seanmeyer544@gmail.com>
2026-07-22 12:22:05 +0200

    bump lib version to 0.2.0, added more ascii matchers, deprecated plain digit matcher

--- src/combinators/ascii.rs
+++ src/combinators/ascii.rs
@@ -30,3 +30,39 @@ where
 {
     ascii_lower().or(ascii_upper())
 }
+
+/// Returns a matcher compatible with inputs that use `char` as symbol. The matcher succeeds on ascii digits.
+#[must_use]
+pub fn ascii_digit<I>() -> impl Matcher<I>
+where
+    I: Input<Symbol = char>,
+{
+    predicate(|symbol: char| symbol.is_ascii_digit())
+}
+
+/// Returns a matcher compatible with inputs that use `char` as symbol. The matcher succeeds on ascii alphanumeric characters.
+#[must_use]
+pub fn ascii_alphanumeric<I>() -> impl Matcher<I>
+where
+    I: Input<Symbol = char>,
+{
+    ascii_alpha().or(ascii_digit())
+}
+
+/// Returns a matcher compatible with inputs that use `char` as symbol. The matcher succeeds on ascii hexdigits.
+#[must_use]
+pub fn ascii_hex<I>() -> impl Matcher<I>
+where
+    I: Input<Symbol = char>,
+{
+    predicate(|symbol: char| symbol.is_ascii_hexdigit())
+}
+
+/// Returns a matcher compatible with inputs that use `char` as symbol. The matcher succeeds on ascii whitespace.
+#[must_use]
+pub fn ascii_whitespace<I>() -> impl Matcher<I>
+where
+    I: Input<Symbol = char>,
+{
+    predicate(|symbol: char| symbol.is_ascii_whitespace())
+}
--- src/combinators/digit.rs
+++ src/combinators/digit.rs
@@ -1,6 +1,7 @@
 use crate::{combinators::predicate::predicate, input::Input, matcher::Matcher};
 
 #[must_use]
+#[deprecated(since = "0.2.0", note = "Use ascii::ascii_digit instead")]
 pub fn digit<I>() -> impl Matcher<I>
 where
     I: Input<Symbol = char>,
